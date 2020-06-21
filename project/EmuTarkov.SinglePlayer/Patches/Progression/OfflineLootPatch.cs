854cecbdd88a48037f2bf826a03c8b3a72acac9b
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-06-21 14:32:10 +0200

    Fix infinite loading

--- project/EmuTarkov.SinglePlayer/Patches/Progression/OfflineLootPatch.cs
+++ project/EmuTarkov.SinglePlayer/Patches/Progression/OfflineLootPatch.cs
@@ -28,7 +28,7 @@ namespace EmuTarkov.SinglePlayer.Patches.Progression
 		{
 			var localGameBaseType = PatcherConstants.LocalGameType.BaseType;
 
-			_property = localGameBaseType.GetProperty($"{nameof(LocationInfo)}_0", BindingFlags.NonPublic | BindingFlags.Instance);
+			_property = localGameBaseType.GetProperty($"{nameof(GClass717.GClass719)}_0", BindingFlags.NonPublic | BindingFlags.Instance);
 			return localGameBaseType.GetMethod(methodName, flags);
 		}
 
