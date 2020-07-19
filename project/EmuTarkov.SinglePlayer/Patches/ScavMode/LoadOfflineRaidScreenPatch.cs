ba87d1821e845e31e020cfc74b40c30ba45fe0c8
PoloYolo <camo1018@yahoo.com>
2020-07-18 22:18:14 -0700

    Revert changes to make LoadOfflineRaidScreenpatch more assembly update friendly.

--- project/EmuTarkov.SinglePlayer/Patches/ScavMode/LoadOfflineRaidScreenPatch.cs
+++ project/EmuTarkov.SinglePlayer/Patches/ScavMode/LoadOfflineRaidScreenPatch.cs
@@ -9,6 +9,7 @@ using EmuTarkov.SinglePlayer.Utils.Reflection;
 using EFT;
 using EFT.UI.Matchmaker;
 using EFT.UI.Screens;
+using MenuController = GClass1101;
 
 namespace EmuTarkov.SinglePlayer.Patches.ScavMode
 {
@@ -20,13 +21,14 @@ namespace EmuTarkov.SinglePlayer.Patches.ScavMode
 
         protected override MethodBase GetTargetMethod()
         {
-            return PatcherConstants.MenuControllerType.GetNestedTypes(BindingFlags.NonPublic)
+            return typeof(MenuController).GetNestedTypes(BindingFlags.NonPublic)
                 .Single(x => x.Name == "Class761")
                 .GetMethod("method_2", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
         }
 
         static IEnumerable<CodeInstruction> PatchTranspiler(IEnumerable<CodeInstruction> instructions)
         {
+
             var codes = new List<CodeInstruction>(instructions);
 
             int index = 29;
@@ -38,46 +40,43 @@ namespace EmuTarkov.SinglePlayer.Patches.ScavMode
             return codes.AsEnumerable();
         }
 
-        private static object GetMenuController()
+        private static MenuController GetMenuController()
         {
             return PrivateValueAccessor.GetPrivateFieldValue(typeof(MainApplication),
-                "gclass1101_0", ClientAppUtils.GetMainApp());
+                "gclass1101_0", ClientAppUtils.GetMainApp()) as MenuController;
         }
 
 
         // Refer to MatchmakerOfflineRaid's subclass's OnShowNextScreen action definitions if these structs numbers change.
         public static void LoadOfflineRaidNextScreen(bool local, GStruct75 weatherSettings, GStruct180 botsSettings, GStruct76 wavesSettings)
         {
-            // MenuController = GClass1101 - 12.6.8170.
-            object menuController = GetMenuController();
-            PropertyInfo locationProperty = menuController.GetType().GetProperty("SelectedLocation");
-            object selectedLocation = locationProperty.GetGetMethod().Invoke(menuController, null);
-            if (selectedLocation.GetType().GetField("Id").GetValue(selectedLocation) as string == "laboratory")
+            MenuController menuController = GetMenuController();
+            if (menuController.SelectedLocation.Id == "laboratory")
             {
                 wavesSettings.IsBosses = true;
             }
-
+            
             SetMenuControllerFieldValue(menuController, "bool_0", local);
 
             SetMenuControllerFieldValue(menuController, "gstruct180_0", botsSettings);
             SetMenuControllerFieldValue(menuController, "gstruct76_0", wavesSettings);
             SetMenuControllerFieldValue(menuController, "gstruct75_0", weatherSettings);
             
-            PatcherConstants.MenuControllerType.GetMethod("method_36", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(menuController, null);
+            typeof(MenuController).GetMethod("method_36", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(menuController, null);
         }
 
         public static void LoadOfflineRaidScreenForScav()
         {
-            object menuController = GetMenuController();
+            MenuController menuController = GetMenuController();
 
             MatchmakerOfflineRaid.GClass1852 gclass = new MatchmakerOfflineRaid.GClass1852();
             gclass.OnShowNextScreen += LoadOfflineRaidNextScreen;
-            gclass.OnShowReadyScreen += (OfflineRaidAction)Delegate.CreateDelegate(typeof(OfflineRaidAction), menuController, "method_54");
+            gclass.OnShowReadyScreen += (OfflineRaidAction)Delegate.CreateDelegate(typeof(OfflineRaidAction), (object)menuController, "method_54");
             gclass.ShowScreen(EScreenState.Queued);
         }
 
-        private static void SetMenuControllerFieldValue(object instance, string fieldName, object value) {
-            PrivateValueAccessor.SetPrivateFieldValue(PatcherConstants.MenuControllerType, fieldName, instance, value);
+        private static void SetMenuControllerFieldValue(MenuController instance, string fieldName, object value) {
+            PrivateValueAccessor.SetPrivateFieldValue(typeof(MenuController), fieldName, instance, value);
         }
     }
 }
